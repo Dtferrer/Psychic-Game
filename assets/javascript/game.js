5aa0cc62530040588c76f9359ad8623647e29213
Danica Ferrer <dtferrer@ucsd.edu>
2019-07-10 01:17:00 -0700

    Initialized HTML and some CSS

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+    // variables //
+
+    var wins = 0;
+    var losses = 0;
+
+    var maxAttempts = 12;
+
+    var words;
+    var chosenWord;
+    var guess;
+    var guesses =[];
+
+    var counter;
+    var space;
+
+    // pressing key //
+    document.onkeyup = function(event) {
+        var userGuess = event.key;
+
+
+
+};
+}
