d7ccf098d6ba1d4d3d85426a211b298c388f96c3
Danica Ferrer <dtferrer@ucsd.edu>
2019-07-12 21:06:45 -0700

    completed game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,94 @@
-window.onload = function() {
-    // variables //
+// variables //
 
     var wins = 0;
-    var losses = 0;
 
-    var maxAttempts = 12;
+    var Attempts = 12;
 
-    var words;
+    var wordslist = ["mario", "sonic", "tetris", "pokemon", "minecraft", "fortnite", "overwatch"]
     var chosenWord;
-    var guess;
     var guesses =[];
 
-    var counter;
     var space;
+    var totalspace;
+
+    // Start
+    function startGame() {
+
+        Attempts = 12;
+
+        chosenWord = wordslist[Math.floor(Math.random() * wordslist.length)];
+
+        space = chosenWord.length;
+
+        console.log(chosenWord);
+        console.log(space);
+
+        totalspace = [];
+
+        guesses = [];
+
+        for (var i=0; i < space; i++) {
+            totalspace.push("_");
+        }
+        console.log(totalspace);
+
+        // setting html to starting conditions
+        document.getElementById("guesses-left").innerHTML = Attempts;
+        document.getElementById("word-display").innerHTML = totalspace.join(" ");
+        document.getElementById("guessed-letters").innerHTML = guesses.join(" ");
+    }
+
+    for (var i = 0; i < chosenWord; i++) {
+        alert(chosenWord.charAt(i));
+    }
 
     // pressing key //
+
+    startGame();
+
     document.onkeyup = function(event) {
-        var userGuess = event.key;
+        var letter = event.key;
+        var isInWord = false;
 
+        console.log(letter);
 
+        // checking if letter in word
+        for (var i = 0; i < chosenWord.length; i++) {
+            if (chosenWord.charAt(i) === letter) {
+                isInWord = true;
+            }
+        }
 
-};
-}
+        if (isInWord == true) {
+            for (var j = 0; j < chosenWord.length; j++) {
+                if (chosenWord.charAt(j) === letter) {
+                    totalspace[j] = letter;
+                }
+            }
+        }
+
+        else {
+            guesses.push(letter);
+            Attempts--;
+        }
+
+        // update conditions of the game
+        document.getElementById("guesses-left").innerHTML = Attempts;
+        document.getElementById("word-display").innerHTML = totalspace.join(" ");
+        document.getElementById("guessed-letters").innerHTML = guesses.join(" ");
+
+        // win or loss
+        if (chosenWord === totalspace.join("")) {
+            wins++;
+            alert("A Winner is You!!!");
+            document.getElementById("win-count").innerHTML = wins;
+            startGame();
+        }
+
+        else if (Attempts === 0) {
+            alert("Sorry, Try Again!");
+            startGame();
+        }
+    }
+    
+    console.log(totalspace);
